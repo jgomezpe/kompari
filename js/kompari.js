ddf65214d4f286d58e3c9823023b9f73ccd49bf9
Jonatan Gomez <jogoper@gmail.com>
2021-08-22 20:57:23 -0500

    Adds online documentation to kompari.js

--- js/kompari.js
+++ js/kompari.js
@@ -14,18 +14,18 @@
 
 /////// Kompari.js ////////////
 /**
- * Determines if the first number is less than (in some order) the second number(one<two)
+ * Determines if the first number is less than (in some order) the second number(one&lt;two)
  * @param one First number
  * @param two Second number
- * @return (one<two)
+ * @return (one&lt;two)
  */
 function l2h(one,two){ return (one-two) }
 
 /**
- * Determines if the first number is greater than (in some order) the second number(one<two)
+ * Determines if the first number is greater than (in some order) the second number(one&gt;two)
  * @param one First number
  * @param two Second number
- * @return (one>two)
+ * @return (one&gt;two)
  */
 function h2l(one,two){ return (two-one) }
 
@@ -36,18 +36,12 @@ Compare = {
     }    
 }
 
-/**
- * <p>Searching algorithm for sorted arrays of objects</p>
- * 
- * <p>Copyright: Copyright (c) 2010</p>
- * 
- * @author Jonatan Gomez Perdomo
- * @version 1.0
- */
+/** Searching algorithm for sorted arrays of objects */
 class SortedSearch {
     /**
      * Creates a search operation for the given sorted array
      * @param sorted Array of elements (should be sorted)
+     * @param order Order used for locating the object
      */
     constructor(order, sorted){ 
         this.order = order
@@ -57,6 +51,8 @@ class SortedSearch {
     /**
      * Searches for the position of the given element. The vector should be sorted
      * @param x Element to be located
+     * @param start Starting searching position
+     * @param end Ending (not included) searching position
      * @return The position of the given object, -1 if the given object is not in the array
      */
     find(x, start, end) { 
@@ -71,6 +67,8 @@ class SortedSearch {
     /**
      * Determines if the sorted array contains the given element (according to the associated order)
      * @param x Element to be located
+     * @param start Starting searching position
+     * @param end Ending (not included) searching position
      * @return <i>true</i> if the element belongs to the sorted array, <i>false</i> otherwise
      */
     contains(x, start, end){ return (this.find(x, start, end) != -1) }
@@ -79,6 +77,8 @@ class SortedSearch {
      * Searches for the position of the first element in the array that is bigger
      * than the element given. The array should be sorted
      * @param x Element to be located
+     * @param start Starting searching position
+     * @param end Ending (not included) searching position
      * @return Position of the object that is bigger than the given element
      */
     findRight(x, start, end){ 
@@ -101,6 +101,8 @@ class SortedSearch {
     /**
      * Searches for the position of the last element in the array that is smaller
      * than the element given. The array should be sorted
+     * @param start Starting searching position
+     * @param end Ending (not included) searching position
      * @param x Element to be located
      * @return Position of the object that is smaller than the given element
      */
